e7c5eaf2424107ee2660a3d1a3614cd15a0e1ad0
Orlando Corona <corona.orlando@gmail.com>
2020-06-28 00:08:23 -0500

    Ep word renders on the DOM and crosschecking funct

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,54 @@
+//I want to create a simple array holding my word options. My theme will be popular sitcoms.
+let shows = ["friends", "seinfeld", "game of thrones"];
+//Now I want to set up variables for randomization and container for current show being used. I will also initiate another var for my score 
+let showIndex = Math.floor(Math.random() * shows.length);
+let word = shows[showIndex];
+let score = 0;
+
+console.log(`Here's my random show: ${word}`);
+
+function renderQuestion() {
+    if (showIndex <= (shows.length - 1)) {
+        document.querySelector("#currentWord").innerHTML = `<h3>${shows[showIndex].toUpperCase()}</h3>`;
+    } else {
+        document.querySelector("#currentWord").innerHTML = `Game Over!\nYour final score is: ${score}`;
+    }
+}
+
+//I will now call my question function to display the current word 
+renderQuestion();
 
 //following function will run when key is typed
 document.onkeyup = function (event) {
-    console.log(event.key);
-    let letter = event.key.toLowerCase();
-    document.getElementById("#lettersGuessed").innerHTML = "test";
+    //This variable will hold the user's on-click input 
+    let userGuess = event.key.toLowerCase();
+    console.log(userGuess);
+
+    let newLetter;
+
+    for (let i = 0; i < word.length; i++) {
+        // console.log(word + " Here's my word");
+        // console.log("Here's my letter " + word.charAt(0));
+        let split = word.split("");
+        console.log(split + " Here is my word split");
+
+        let letter = word.charAt(i);
+        while (condition) {
+
+            if (userGuess === letter) {
+                console.log("Your letter matches: " + letter);
+
+            }
+        };
+        // if(letter === userGuess){
+        //     console.log(`Here's your letter ${letter}`);
+
+        // }
+        // newLetter = letter;
+
+    }
+    // console.log("new letter: " + newLetter);
+
+    //Create a for-loop that keeps track of my word and will cross check each of the letters one by one d5
+    // document.getElementById("#lettersGuessed").innerHTML = "test";
 } 
