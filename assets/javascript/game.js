2b41d25ef34c7db9ed12088c4ca150e302d0d89b
Orlando Corona <corona.orlando@gmail.com>
2020-06-29 11:31:15 -0500

    Cleaned up my code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,9 +3,14 @@ let shows = ["friends", "seinfeld", "game of thrones"];
 //Now I want to set up variables for randomization and container for current show being used. I will also initiate another var for my score 
 let showIndex = Math.floor(Math.random() * shows.length);
 let word = shows[showIndex];
+// let wordIndex = 0;
 let score = 0;
 
+//This var will hold my episode word while also splitting it
+let split = word.split("");
+
 console.log(`Here's my random show: ${word}`);
+console.log(`Here it is split: ${split}`);
 
 function renderQuestion() {
     if (showIndex <= (shows.length - 1)) {
@@ -24,30 +29,22 @@ document.onkeyup = function (event) {
     let userGuess = event.key.toLowerCase();
     console.log(userGuess);
 
-    let newLetter;
-
     for (let i = 0; i < word.length; i++) {
-        // console.log(word + " Here's my word");
         // console.log("Here's my letter " + word.charAt(0));
-        let split = word.split("");
-        console.log(split + " Here is my word split");
-
+        
         let letter = word.charAt(i);
-        while (condition) {
+        // while (word.length) {
 
-            if (userGuess === letter) {
-                console.log("Your letter matches: " + letter);
+        if (userGuess === letter) {
+            console.log("Your letter matches: " + letter);
+            document.querySelector(`#currentWord`).innerHTML = `${letter}`
 
-            }
-        };
-        // if(letter === userGuess){
-        //     console.log(`Here's your letter ${letter}`);
+        }
+        // };
 
-        // }
         // newLetter = letter;
 
     }
-    // console.log("new letter: " + newLetter);
 
     //Create a for-loop that keeps track of my word and will cross check each of the letters one by one d5
     // document.getElementById("#lettersGuessed").innerHTML = "test";
